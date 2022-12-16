355fff5809aba897aa08cb14a2f8af9e15472820
Rjkirkhope <rjkirkhope@gmail.com>
2022-12-15 20:44:54 -0700

    Broke CSS out into smaller helper files for readability, cleaned up JS and added doc blocks to functions for quick reference. Added more media break points for scaling. A few small tweaks to go, WIP

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function init(){
 
+  /**
+   * Mock Data source of products and banner details
+   */
   const developmentData = { 
   pageBannerImageDesktop : "image-assets/icons-remastered-online-lp-d.jpg",
   pageBannerImageMobile : "image-assets/icons-remastered-online-lp-m.jpg",
@@ -9,8 +12,8 @@ function init(){
   productHeadline: 'Nuptse Jacket',
   productAttention: 'Remastered',
   productCopy: 'You\'ve seen this jacket. What you haven\'t <br> seen is the innovative:new technology, <br> elevated bonded baffle construction,<br> premium materials. This is not the <br> Nuptse you know. It\'s the Nuptse like <br> never before.',
-  productButtons: ['Shop Mens', 'Shop Womens'],
-  productURL: 'https://www.thenorthface.com/en-us/womens/collections/summit-series-c324773/summit-torre-egger-futurelight-boots-pNF0A52RE?color=KX9',
+  productURLMens: 'https://www.thenorthface.com/en-us/womens/collections/summit-series-c324773/summit-torre-egger-futurelight-boots-pNF0A52RE?color=KX9',
+  productURLWomens: 'https://www.thenorthface.com/en-us/womens/collections/summit-series-c324773/summit-torre-egger-futurelight-boots-pNF0A52RE?color=KX9',
   productImageDesktop: 'image-assets/icons-remastered-online-lp-rmst-nuptse-d.jpg',
   productImageMobile: 'image-assets/icons-remastered-online-lp-rmst-nuptse-m.jpg',
   productImageAlt: 'Nuptse Jacket'
@@ -19,14 +22,19 @@ function init(){
   productHeadline: 'Nuptse Jacket',
   productAttention: 'Remastered',
   productCopy: 'You\'ve seen this jacket. What you haven\'t <br> seen is the innovative:new technology, <br> elevated bonded baffle construction,<br> premium materials. This is not the <br> Nuptse you know. It\'s the Nuptse like <br> never before.',
-  productButtons: ['Shop Mens', 'Shop Womens'],
-  productURL: 'https://www.thenorthface.com/en-us/womens/collections/summit-series-c324773/summit-torre-egger-futurelight-boots-pNF0A52RE?color=KX9',
+  productURLMens: 'https://www.thenorthface.com/en-us/womens/collections/summit-series-c324773/summit-torre-egger-futurelight-boots-pNF0A52RE?color=KX9',
+  productURLWomens: 'https://www.thenorthface.com/en-us/womens/collections/summit-series-c324773/summit-torre-egger-futurelight-boots-pNF0A52RE?color=KX9',
   productImageDesktop: 'image-assets/icons-remastered-online-lp-rmst-nuptse-d.jpg',
   productImageMobile: 'image-assets/icons-remastered-online-lp-rmst-nuptse-m.jpg',
   productImageAlt: 'Nuptse Jacket'
   }]
 } 
 
+/**
+ * Function to handle appending banner data to the page
+ * 
+ * @param developmentData
+ */
 function buildBanner(developmentData) {
   document.getElementById('bannerHeadline').innerHTML=`<h1>${developmentData.bannerHeadline}</h1>`;
   document.getElementById('bannerCopy').innerHTML=`<p>${developmentData.bannerCopy}</p>`;
@@ -35,6 +43,12 @@ function buildBanner(developmentData) {
 buildBanner(developmentData)
 mapProductObjects(developmentData)
 
+/**
+ * Function to handle if a product image should be placed on the right or left of the page,
+ * is scalable for multiple products
+ * 
+ * @param developmentData
+ */
 function mapProductObjects(developmentData) {
   developmentData.productObjects.map((productObject, index) => {
     if (index % 2 === 0) {
@@ -45,8 +59,12 @@ function mapProductObjects(developmentData) {
   })
 }
 
+/**
+ * Fucntion to handle if a product has image data on the right and text data on the left
+ * 
+ * @param productObject
+ */
 function buildProductImageRight(productObject) {
-
   const productDetails = document.getElementById('productSection');
   productDetails.innerHTML += 
     `<div class='productRight'>
@@ -56,29 +74,35 @@ function buildProductImageRight(productObject) {
           <h2 class='productHeadline'>${productObject.productHeadline}</h2>
           <p class='productCopy'>${productObject.productCopy}</p>
         <div class="buttonContainer">
-          <a href="${productObject.productURL}"><button >Shop Women's</button></a>
-          <a href="${productObject.productURL}"><button >Shop Men's</button></a>
+          <a href="${productObject.productURLWomens}"><button >Shop Women's</button></a>
+          <a href="${productObject.productURLMens}"><button >Shop Men's</button></a>
         </div>
         </div>
       </div>
-      <img class="productImage" src="image-assets/icons-remastered-online-lp-rmst-nuptse-m.jpg" alt="nuptse jacket" width="100%">
+      <img class="productImageDesktop" src="${productObject.productImageDesktop}" alt="nuptse jacket" width="100%">
+      <img class="productImageMobile" src="${productObject.productImageMobile}" alt="nuptse jacket" width="100%">
     </div>`  
 }
 
+/**
+ * Function to handle if a product has image data on the left and text data on the right
+ * 
+ * @param productObject
+ */
 function buildProductImageLeft(productObject) {
-
   const productDetails = document.getElementById('productSection');
   productDetails.innerHTML += 
     `<div class='productLeft'>
-      <img class="productImage" src="image-assets/icons-remastered-online-lp-rmst-nuptse-m.jpg" alt="nuptse jacket" width="100%">
+    <img class="productImageDesktop" src="${productObject.productImageDesktop}" alt="nuptse jacket" width="100%">
+      <img class="productImageMobile" src="${productObject.productImageMobile}" alt="nuptse jacket" width="100%">
       <div class='productTextContainer'>
         <div class= 'productText'>
           <p class='productAttention'>${productObject.productAttention}<p>
           <h2 class='productHeadline'>${productObject.productHeadline}</h2>
           <p class="productCopy">${productObject.productCopy}</p>
         <div class="buttonContainer">
-          <a href="${productObject.productURL}"><button>Shop Women's</button></a>
-          <a href="${productObject.productURL}"><button>Shop Men's</button></a>
+          <a href="${productObject.productURLWomens}"><button>Shop Women's</button></a>
+          <a href="${productObject.productURLMens}"><button>Shop Men's</button></a>
         </div>
         </div>
       </div>
